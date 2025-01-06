0fa631d015e6a99f0a801363bf3e82e98bea0136
clintc1 <191350491+clintc1@users.noreply.github.com>
2025-01-06 02:06:53 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://clintc1.github.io',
+    site: 'https://clintc1.github.io/ccweb',
     integrations: [
         mdx(),
         sitemap(),
