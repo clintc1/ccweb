bf5e6931df92d9303ac2f7f33fe975fd8927d6e8
clintc1 <191350491+clintc1@users.noreply.github.com>
2026-04-22 12:35:14 +0100

    fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,7 @@
-import { defineConfig } from 'astro/config';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import tailwindcss from '@tailwindcss/vite';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-
 export default defineConfig({
-  vite: {
-    plugins: [tsconfigPaths(), tailwindcss()],
-  },
-  site: 'https://clintoncawood.com',
-  integrations: [mdx(), sitemap()],
-});
+    vite: {
+      resolve: { tsconfigPaths: {} },
+      plugins: [tsconfigPaths(), tailwindcss()],
+    },
+    ...
+  });
