7517941458ca09971ad3c4df0f664ca6830b6904
clintc1 <191350491+clintc1@users.noreply.github.com>
2026-04-22 11:57:09 +0100

    vite etc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+    vite: {
+        plugins: [tailwindcss()],
+      },
     site: 'https://clintoncawood.com',
     integrations: [
         mdx(),
